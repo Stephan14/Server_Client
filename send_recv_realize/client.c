34a73e05ff6337ddab978b3006bec225ca79fc2f
Stephan14 <zsf657135507@gmail.com>
2016-04-09 20:50:41 +0800

    排版添加缩进

--- send_recv_realize/client.c
+++ send_recv_realize/client.c
@@ -8,7 +8,6 @@
 
 #define MAXLINE 4096
 
-
 int main(int argc, char** argv)
 {
     int    sockfd, n,rec_len;
@@ -16,47 +15,54 @@ int main(int argc, char** argv)
     char    buf[MAXLINE];
     struct sockaddr_in    servaddr;
 
-
-    if( argc != 2){
-    printf("usage: ./client <ipaddress>\n");
-    exit(0);
+    if( argc != 2)
+    {
+      printf("usage: ./client <ipaddress>\n");
+      exit(0);
     }
 
 
-    if( (sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0){
-    printf("create socket error: %s(errno: %d)\n", strerror(errno),errno);
-    exit(0);
+    if( (sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+      printf("create socket error: %s(errno: %d)\n", strerror(errno),errno);
+      exit(0);
     }
 
 
     memset(&servaddr, 0, sizeof(servaddr));
     servaddr.sin_family = AF_INET;
     servaddr.sin_port = htons(8000);
-    if( inet_pton(AF_INET, argv[1], &servaddr.sin_addr) <= 0){
+    if( inet_pton(AF_INET, argv[1], &servaddr.sin_addr) <= 0)
+    {
       printf("inet_pton error for %s\n",argv[1]);
       exit(0);
     }
 
 
-    if( connect(sockfd, (struct sockaddr*)&servaddr, sizeof(servaddr)) < 0){
-    printf("connect error: %s(errno: %d)\n",strerror(errno),errno);
-    exit(0);
+    if( connect(sockfd, (struct sockaddr*)&servaddr, sizeof(servaddr)) < 0)
+    {
+      printf("connect error: %s(errno: %d)\n",strerror(errno),errno);
+      exit(0);
     }
-
 
     printf("send msg to server: \n");
     fgets(sendline, 4096, stdin);
+
     if( send(sockfd, sendline, strlen(sendline), 0) < 0)
     {
-    printf("send msg error: %s(errno: %d)\n", strerror(errno), errno);
-    exit(0);
+      printf("send msg error: %s(errno: %d)\n", strerror(errno), errno);
+      exit(0);
     }
-    if((rec_len = recv(sockfd, buf, MAXLINE,0)) == -1) {
+
+    if((rec_len = recv(sockfd, buf, MAXLINE,0)) == -1)
+    {
        perror("recv error");
        exit(1);
     }
+
     buf[rec_len]  = '\0';
     printf("Received : %s ",buf);
+
     close(sockfd);
-    exit(0);
+    return 0;
 }
